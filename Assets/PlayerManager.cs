6f5099a7cf200361f55a074a42964d40504aa6da
cdjones03 <cjone174@depaul.edu>
2025-01-23 14:35:38 -0600

    added coin prefab and initial mood slider

--- Assets/PlayerManager.cs
+++ Assets/PlayerManager.cs
@@ -1,16 +1,61 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
+    public int curMoodValue;
+    public int maxMoodValue;
+
+    public int curCoins;
+    public int income;
+
+    public TextMeshProUGUI curCoinsText;
+    public Slider moodSlider;
+
+    private float moodTimer = 0f;
+    private float moodDecreaseInterval = 10f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        curCoins = 0;
+        curCoinsText.text = curCoins.ToString();
+
+        maxMoodValue = 10;
+        curMoodValue = maxMoodValue;
+        moodSlider.value = curMoodValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Handle mood decrease timer
+        moodTimer += Time.deltaTime;
+        if (moodTimer >= moodDecreaseInterval)
+        {
+            moodTimer = 0f;  // Reset timer
+            AddMood(-1);     // Decrease mood by 1
+            moodSlider.value = curMoodValue;  // Update the slider
+        }
+    }
+
+    public void AddMood(int amount)
+    {
+        curMoodValue += amount;
+        if (curMoodValue > maxMoodValue)
+        {
+            curMoodValue = maxMoodValue;
+        }
+        else if (curMoodValue < 0)  // Added to prevent negative mood
+        {
+            curMoodValue = 0;
+        }
+        moodSlider.value = curMoodValue;  // Update slider whenever mood changes
+    }
+
+    public void AddCoin(){
+        curCoins += 1;
+        curCoinsText.text = curCoins.ToString();
     }
 }
